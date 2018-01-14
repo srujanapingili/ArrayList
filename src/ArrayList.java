9950b67901dca516e67ae02abc6a5be9834c2b32
pingili srujana <srujanareddy9959@gmail.com>
2018-01-13 18:24:39 -0600

    First commit

---
+++ src/ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class A {
+
+	public static void main(String[] args) {
+ArrayList<string> friends= new ArrayList<string>();
+	friends.add("srujana");
+	friends.add("ujwala");
+	System.out.println(friends);
+	}
+
+}
